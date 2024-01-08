b51e1c695b31cd0e54b32159e3ee8159a84229b1
Victor <115024032+VictorDuartCalamot@users.noreply.github.com>
2024-01-08 17:46:33 +0100

    Delete fb directory

--- fb/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-const admin = require('react-admin-firebase');
-const serviceAccount = require('./firebase_key.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://ease-8d84a-default-rtdb.europe-west1.firebasedatabase.app', // replace with your database URL
-});
-
