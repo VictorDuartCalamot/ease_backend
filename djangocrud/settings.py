0ec185d24275c67f6010685752e75be6ce874588
Victor <victor.duart@ieselcalamot.com>
2024-02-15 16:54:32 +0100

    update static

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -149,10 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
