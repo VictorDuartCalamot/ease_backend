fc547a7d338dae5760e2753a8e946ede2914c0a2
Victor <victor.duart@ieselcalamot.com>
2024-02-22 16:19:20 +0100

    identation problem

--- backend/views/management_views.py
+++ backend/views/management_views.py
@@ -58,28 +58,28 @@ class ExpenseView(APIView):
         serializer = ExpenseSerializer(expense)
         return Response(serializer.data)
     
-def post(self, request):
-        # Retrieve the token from the request headers
-        token = request.headers.get('Authorization').split(' ')[1]
-        # Retrieve the user associated with the token
-        try:
-            token_obj = Token.objects.get(key=token)
-            user = token_obj.user
-        except Token.DoesNotExist:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
-        print(user)
-        # Assign the authenticated user to the request data
-        request.data['user'] = user.id
-        # Create a serializer instance with the modified request data
-        serializer = ExpenseSerializer(data=request.data)
-        # Validate the serializer data
-        if serializer.is_valid():
-            # Save the serializer data to the database
-            serializer.save()
-            # Return a success response
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # Return an error response if validation fails
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+            # Retrieve the token from the request headers
+            token = request.headers.get('Authorization').split(' ')[1]
+            # Retrieve the user associated with the token
+            try:
+                token_obj = Token.objects.get(key=token)
+                user = token_obj.user
+            except Token.DoesNotExist:
+                return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+            print(user)
+            # Assign the authenticated user to the request data
+            request.data['user'] = user.id
+            # Create a serializer instance with the modified request data
+            serializer = ExpenseSerializer(data=request.data)
+            # Validate the serializer data
+            if serializer.is_valid():
+                # Save the serializer data to the database
+                serializer.save()
+                # Return a success response
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # Return an error response if validation fails
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request, pk):
         expense = self.get_object(pk)
