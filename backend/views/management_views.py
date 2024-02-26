20b07131172ea310f5c2e13f18ad01482036e53c
Victor <victor.duart@ieselcalamot.com>
2024-02-26 21:08:17 +0100

    dsa

--- backend/views/management_views.py
+++ backend/views/management_views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from backend.models import Income, Expense, Expense2
+from backend.models import Income, Expense
 from backend.serializers import IncomeSerializer, ExpenseSerializer,Expense2Serializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
