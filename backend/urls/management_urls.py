854684ac6798010ff9f776215f06e057f3401a35
Victor <victor.duart@ieselcalamot.com>
2024-02-26 20:38:13 +0100

    123aed

--- backend/urls/management_urls.py
+++ backend/urls/management_urls.py
@@ -4,8 +4,8 @@ from backend.views import management_views as management_views
 urlpatterns = [
     path('income/', management_views.IncomeView.as_view(), name='income-list'),
     path('expense/', management_views.ExpenseView.as_view(), name='expense-list'),
-    path('expenses/', management_views.ExpenseListCreateAPIView.as_view(), name='expense-list-create'),
-    path('expenses/<int:pk>/', management_views.ExpenseRetrieveUpdateDestroyAPIView.as_view(), name='expense-detail'),
+    path('expenses2/', management_views.ExpenseListCreateAPIView.as_view(), name='expense-list-create'),
+    path('expenses2/<int:pk>/', management_views.ExpenseRetrieveUpdateDestroyAPIView.as_view(), name='expense-detail'),
     
 ]
 
