71d58086fa2741c5dd240b39b6c971807d539771
Victor <victor.duart@ieselcalamot.com>
2024-03-05 16:31:34 +0100

    debug

--- backend/serializers.py
+++ backend/serializers.py
@@ -38,7 +38,7 @@ class UserSerializerWithToken(UserSerializer):
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    user = serializers.PrimaryKeyRelatedField(queryset=User.pk)    
     class Meta:
         model = Expense
         fields = ['id', 'amount', 'category', 'creation_date', 'user']
