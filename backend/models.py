7ba400a3029b54f8d72c6a9d6b24f22adda6eae8
Victor <victor.duart@ieselcalamot.com>
2024-03-11 20:30:03 +0100

    updated model

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from backend.utils import generate_random_id
+import uuid
 #Income/Expense models
 """
 class auth_user_logs(models.Model):
@@ -12,16 +13,17 @@ class auth_user_logs(models.Model):
 
     
 class Expense(models.Model):
-    id = models.CharField(('expense_id'), max_length=50, unique=True,primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #_id = models.CharField(('expense_id'), max_length=50, unique=True,primary_key=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100)
     creation_date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
-    def save(self, *args, **kwargs):
+    """def save(self, *args, **kwargs):
         print("Entro en el save del modelo")                
-        self.id = generate_random_id('E-')
+        #self.id = generate_random_id('E-')
         print("ID: "+ str(self.id))
         object = super().save(*args, **kwargs)        
         print("Despues de guardar el objeto" + str(object))
-        return object
+        return object"""
