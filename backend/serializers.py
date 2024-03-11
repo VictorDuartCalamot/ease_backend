c1b84fafb9c7877858df4a0ee59cd10a837a2c27
Victor <victor.duart@ieselcalamot.com>
2024-03-11 16:18:51 +0100

    debug

--- backend/serializers.py
+++ backend/serializers.py
@@ -46,16 +46,14 @@ class ExpenseSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         print("He entrado en el create dentro del serializer y antes de hacer el self.context")
-        # Retrieve the user from the context
-        print(self.context.get('user'))
+        # Retrieve the user from the context        
         try:
             user_pk = self.context.get('user')
-            print(user_pk)  # Verify if this prints anything
+            print(user_pk + " Print userpk")  # Verify if this prints anything
     # Add other operations with user_pk here
         except Exception as e:
             print(f"Error retrieving user_pk: {str(e)}")
-        print(self.context.get('user'))
-        user_pk = self.context.get('user')
+                            
         print("Despues de hacer el self.context El usuario: "+user_pk)
         # Add the user to the validated data before saving
         validated_data['user'] = user_pk
