678191a483614c3b506838587b5615a78ec69fae
Victor <victor.duart@ieselcalamot.com>
2024-03-12 16:32:35 +0100

    static debug

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -151,9 +151,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-if not DEBUG:    
-    STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+if not DEBUG:        
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
