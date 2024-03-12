a5240064d9ecababc12d96be64a65204ef8bcadc
Victor <victor.duart@ieselcalamot.com>
2024-03-12 16:30:36 +0100

    debug True

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-
+#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
