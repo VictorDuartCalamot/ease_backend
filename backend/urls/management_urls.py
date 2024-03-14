aacf240f23e76caded35f8deb6ebb7646b8a8bbc
Victor <victor.duart@ieselcalamot.com>
2024-03-14 18:45:00 +0100

    modified url

--- backend/urls/management_urls.py
+++ backend/urls/management_urls.py
@@ -3,7 +3,7 @@ from backend.views import management_views as management_views
 
 urlpatterns = [
     #path('income/', management_views.IncomeView.as_view(), name='income-list'),
-    path('expense/', management_views.ExpenseView.as_view({'post': 'create'},{'delete':'delete'}), name='expense-list'),
+    path('expense/', management_views.ExpenseView.as_view({'post': 'create','delete':'delete'}), name='expense-list'),
     
 ]
 
