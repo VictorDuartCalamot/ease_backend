5c11157a90f35b187c998af9fcacfae395a80050
Victor <victor.duart@ieselcalamot.com>
2024-03-14 17:39:42 +0100

    forgot to update serializer

--- backend/serializers.py
+++ backend/serializers.py
@@ -41,7 +41,7 @@ class ExpenseSerializer(serializers.ModelSerializer):
     #user = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     class Meta:
         model = Expense
-        fields = ['id', 'amount', 'category', 'creation_date', 'user_id']
+        fields = ['id', 'amount', 'category', 'creation_date', 'user']
         read_only_fields = ['id']
         
         '''def create(self,validated_data):
