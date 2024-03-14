5025af26512186068ef00118c3e94d6d3c2a49b8
Victor <victor.duart@ieselcalamot.com>
2024-03-14 18:03:49 +0100

    added delete mode

--- backend/views/management_views.py
+++ backend/views/management_views.py
@@ -40,26 +40,33 @@ class ExpenseView(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        print("Entered the post method")
-        
+    def create(self, request, *args, **kwargs):                
         # Ensure the user is authenticated
         if not request.user.is_authenticated:
-            return Response({"error": "User is not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        # Assign the user to the expense
-        request.data['user'] = request.user.id
-        
-        # Print request data for debugging
-        print("Request data:", request.data)
-        
-        # Create a serializer instance with the request data
+            return Response({"error": "User is not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)            
+        #Insert userID into the request.data array
+        request.data['user'] = request.user.id                        
+        # Create a serializer instance with the data in the array
         serializer = ExpenseSerializer(data=request.data) 
-        
-        if serializer.is_valid():
-            print('Serializer is valid')
+        #Check if the serializer is valid
+        if serializer.is_valid():            
             serializer.save()  # Save the expense object to the database
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             print(serializer.errors)  # Print out the errors for debugging
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request):
+        expense_object = self.get_object(request.data.id)
+        expense_object.delete()
+        try: 
+            self.get_object(request.data.id)
+            # If the object still exists after deletion, return 202 Accepted
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except Expense.DoesNotExist:
+            # If the object doesn't exist after deletion, return 204 No Content
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
+
+        
