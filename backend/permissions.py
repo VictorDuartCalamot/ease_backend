592fe798d154d2515aa87883ce6eaade8b87c6ca
Victor <victor.duart@ieselcalamot.com>
2024-03-14 19:15:01 +0100

    updated Expense URLS and the code of view

--- backend/permissions.py
+++ backend/permissions.py
@@ -1,6 +1,18 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to delete it.
+    
+    """
+
     def has_object_permission(self, request, view, obj):
-        
-        return obj.user == request.user
+        # Allow GET, HEAD, and OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Check if the user is the owner of the expense.
+        if obj.user == request.user:
+            return True
+        else:
+            return False
