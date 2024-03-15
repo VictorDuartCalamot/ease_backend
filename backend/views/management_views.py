9386126f2fb03d2dc89bc0aec82ff0a9afbdfe56
Victor <victor.duart@ieselcalamot.com>
2024-03-15 12:27:36 +0100

    debug

--- backend/views/management_views.py
+++ backend/views/management_views.py
@@ -30,7 +30,7 @@ class ExpenseListView(viewsets.ModelViewSet):
         end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date() if end_date_str else None
 
         # Get expenses for the authenticated user
-        user_expenses = Expense.objects.filter(user=request.user)
+        user_expenses = Expense.objects.filter(user=request.user.id)
 
         # Filter expenses based on date range if provided
         if start_date is not None and end_date is not None:
