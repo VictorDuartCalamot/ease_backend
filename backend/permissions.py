fa89cfdbd77916cefb5894350ebcb4eceee35033
Victor <victor.duart@ieselcalamot.com>
2024-03-20 15:56:09 +0100

    debug

--- backend/permissions.py
+++ backend/permissions.py
@@ -9,8 +9,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Allow GET, HEAD, and OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-        print('Obj.user: ',obj.user)
-        print('request.user', request.user)
+        print('Obj.user: ',obj.user.id)
+        print('request.user', request.user.id)
         # Check if the user is the owner of the expense.
         if obj.user == request.user:
             return True
