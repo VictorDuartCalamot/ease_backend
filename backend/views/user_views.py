41252de645b20042bae6ca95b64fa923e118b787
Victor <victor.duart@ieselcalamot.com>
2024-04-10 17:48:37 +0200

    debug

--- backend/views/user_views.py
+++ backend/views/user_views.py
@@ -217,7 +217,7 @@ class AuthUserLogsDetailView(viewsets.ModelViewSet):
 
 
 class SuperAdminManagementDetailView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,PermissionLevel]
     def createUserWithRoles(self,request):
         print('----------------------------')
         print(request.data)
