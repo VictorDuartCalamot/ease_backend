e26e47833e16d70de408e6c808a02063e3dee517
Victor <victor.duart@ieselcalamot.com>
2024-04-10 16:52:47 +0200

    debug

--- backend/urls/admin_urls.py
+++ backend/urls/admin_urls.py
@@ -3,6 +3,6 @@ from backend.views import user_views as user_views
 
 
 urlpatterns = [
-    path('management/user/', user_views.SuperAdminManagementDetailView.as_view({'post':'createUserWithRoles','delete':'','put':''}), name='admin-register'),
+    path('management/user/', user_views.SuperAdminManagementDetailView.as_view({'post':'createUserWithRoles','delete':'','put':''}), name='superuser-tools'),
     
 ]
--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('adminsite/', admin.site.urls),
-    path('admin/', include('backend.urls.admin_urls')),
+    path('api/admin/', include('backend.urls.admin_urls')),
     path('api/users/', include('backend.urls.user_urls')),
     path('api/management/', include('backend.urls.management_urls')),   
 ]
