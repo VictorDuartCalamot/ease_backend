62dc19e0f37bb3f600b5aec36b37e25553373906
Victor <victor.duart@ieselcalamot.com>
2024-04-12 16:33:01 +0200

    debug

--- backend/urls/admin_urls.py
+++ backend/urls/admin_urls.py
@@ -4,6 +4,6 @@ from backend.views import user_views as user_views
 
 urlpatterns = [
     path('user/', user_views.SuperAdminManagementListView.as_view({'get':'getAllUsers','post':'createUserWithRoles'}), name='superuser-tools'),
-    path('user/<int:pk>/', user_views.SuperAdminManagementDetailView.as_view({'delete':'deleteUser','update':'updateUser'}), name='superuser-tools2'),
+    path('user/<int:pk>/', user_views.SuperAdminManagementDetailView.as_view({'delete':'deleteUser','put':'updateUser'}), name='superuser-tools2'),
     
 ]
