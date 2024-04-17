6d3386882b1b890fce78da05512903f780abebb2
Victor <victor.duart@ieselcalamot.com>
2024-04-17 13:42:14 +0200

    debug

--- backend/permissions.py
+++ backend/permissions.py
@@ -41,32 +41,8 @@ class IsSuperUser(permissions.BasePermission):
 class HasEnoughPerms(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         print('Inside has object permission def')
-        # This method is called for object-level permission checks.
-        # You can perform checks here that are specific to the object being accessed.
-
-        # Get the user model
-        User = get_user_model()
-
-        # Check if the request.user is a superuser
-        is_superuser = request.user.is_superuser
-
-        # Check if the object being managed is a staff member
-        if isinstance(obj, User):
-            is_staff = obj.is_staff
-            is_superuser = obj.is_superuser
-        else:
-            is_staff = getattr(obj, 'is_staff', False)
-
-        # Regular users (non-staff) cannot manage other users
-        if not request.user.is_staff:
-            return False
-
-        # Staff members can manage users but not other staff members or superusers
-        if is_staff:
-            return not is_superuser and not request.user.is_superuser
-
-        # Superusers can manage anything
-        return True
+        print(request.user)
+        print(obj)
 
                 
             
