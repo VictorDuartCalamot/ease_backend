2643b2c13081fb34f9139f18ff066204729cd702
Victor <victor.duart@ieselcalamot.com>
2024-04-18 16:40:10 +0200

    debug testing

--- backend/permissions.py
+++ backend/permissions.py
@@ -1,6 +1,9 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+from backend.serializers import UserSerializer
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to delete it.
@@ -38,19 +41,24 @@ class IsSuperUser(permissions.BasePermission):
     
 class HasEnoughPerms(permissions.BasePermission):    
     '''Rule to check if user has enough permissions'''
-    print('Entra siquiera? ? ? ? ?')
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-    def has_object_permission(self, request, view, obj):
-        print('Pero entra dentro??')
+    def isSuperUser(self, user):
+        '''Check if user is super user'''      
+        return user.is_superuser
+    def isStaff(self, user):
+        '''Check if user is super user'''      
+        return user.is_staff
         
-        # Allow GET, HEAD, and OPTIONS requests.                
-        # Check if the user is the owner of the expense.
-        
-        if request.user.is_superuser:
+    def has_permission(self, request, view,pk):
+        user = request.user
+        userObj = User.objects.get(id=pk)
+        serializer = UserSerializer(userObj)  
+        print(serializer.data)
+        if self.isSuperUser(user):            
+            print('User is a super user')
             return True
-        elif request.user.is_staff and (obj.user.is_superuser or obj.user.is_staff) == False:
+        elif self.isStaff(user):
+            print('User is a staff user')
             return True
         else:
+            print('User is not a super user or staff user')
             return False
-        
