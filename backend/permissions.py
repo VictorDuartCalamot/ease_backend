b46b38309ae29b32b67cf52e72383246ebbaf6f1
Victor <victor.duart@ieselcalamot.com>
2024-04-19 12:03:00 +0200

    test and debug perms

--- backend/permissions.py
+++ backend/permissions.py
@@ -2,6 +2,8 @@ from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from backend.models import Income,Expense
+from backend.serializers import IncomeSerializer,ExpenseSerializer
 
 from backend.serializers import UserSerializer
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -10,12 +12,33 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     
     """
     def has_permission(self, request, view):
+        # Determine the type of object (income or expense) based on the view name
+        if 'income' in view.get_view_name().lower():
+            obj_model = Income
+        elif 'expense' in view.get_view_name().lower():
+            obj_model = Expense
+        else:
+            # If the object type cannot be determined, deny permission
+            return False
+        
+        # Retrieve the object based on the primary key (pk) from the request URL
+        user_pk = view.kwargs.get('pk')
+        try:
+            obj = obj_model.objects.get(pk=user_pk)
+            serializer = IncomeSerializer(obj) if obj_model == Income else ExpenseSerializer(obj)
+            print(serializer.data)
+        except obj_model.DoesNotExist:
+            return False        
+        # Check if the user is authenticated
         return request.user.is_authenticated
     
     def has_object_permission(self, request, view, obj):
-        # Allow GET, HEAD, and OPTIONS requests.        
-        # Check if the user is the owner of the expense.
-        if obj.user == request.user:
+        # Allow GET, HEAD, and OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Check if the user is the owner of the object.
+        if obj.user == request.user or obj.user_id == request.user.id:
             return True
         else:
             raise PermissionDenied("You do not have enough permissions.")
@@ -49,9 +72,7 @@ def isStaff(self, user):
     return user.is_staff
         '''
 class HasEnoughPerms(permissions.BasePermission):    
-    '''Rule to check if user has enough permissions'''
-    
-        
+    '''Checks user permission and object permission'''            
     def has_permission(self, request, view):
         user_pk = view.kwargs.get('pk')
         try:
