59c529bec9c833f4484f24814c3aa6fbb52a86a0
Victor <victor.duart@ieselcalamot.com>
2024-05-10 16:05:55 +0200

    fix?

--- djangocrud/asgi.py
+++ djangocrud/asgi.py
@@ -1,22 +1,22 @@
 """
 ASGI config for djangocrud project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``backend``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import django
-from django.urls import path
-from backend.routing import websocket_urlpatterns  # Asegúrate de tener esto configurado en tu aplicación
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
 django.setup()
+
+from backend.routing import websocket_urlpatterns  # Import después de django.setup()
 
 backend = ProtocolTypeRouter({
     "http": get_asgi_application(),  # Define cómo manejar los protocolos HTTP
