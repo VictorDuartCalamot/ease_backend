0671079686053ef8c07078bc7e734786eea3073b
Victor <victor.duart@ieselcalamot.com>
2024-05-13 13:38:08 +0200

    debug

--- backend/consumers.py
+++ backend/consumers.py
@@ -51,8 +51,9 @@ class TechSupportConsumer(AsyncWebsocketConsumer):
     def authenticate_chat(self):
         try:
             print('Mirando si las cosas concuerdan:')
-            print('Self.scope user',self.scope['user'],'chat.consumer',chat['consumer'],'self.scope admin',self.scope['user'],'chat.admin',chat['admin'])
+            
             chat = ChatSession.objects.get(id=self.chat_id, is_active=True)
+            print('Self.scope user',self.scope['user'],'chat.consumer',chat['consumer'],chat.customer,'self.scope admin',self.scope['user'],'chat.admin',chat['admin'],chat.admin)
             print('Chat object:', chat)
             auth = self.scope['user'] == chat.customer or self.scope['user'] == chat.admin
             #logger.debug(f"Authentication for chat {self.chat_id} with user {self.scope['user']} : {auth}")
