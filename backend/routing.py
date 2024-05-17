b69555b671c3d56e1eb0d8a7a8bc7f37ecd7482c
Victor <victor.duart@ieselcalamot.com>
2024-05-17 20:57:23 +0200

    left a /

--- backend/routing.py
+++ backend/routing.py
@@ -4,5 +4,5 @@ from backend.consumers.chat import ChatConsumer
     Route for the websocket connection
 '''
 websocket_urlpatterns = [
-    re_path('ws/support/chat/<uuid:chat_uuid>', ChatConsumer.as_asgi()),
+    re_path('ws/support/chat/<uuid:chat_uuid>/', ChatConsumer.as_asgi()),
 ]
