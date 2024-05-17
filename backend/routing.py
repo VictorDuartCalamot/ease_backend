327ebb1b2d8962f8836b63f4f36576f21bad5692
Victor <victor.duart@ieselcalamot.com>
2024-05-17 20:48:27 +0200

    debug changed url to a one esasier to understand and concise

--- backend/routing.py
+++ backend/routing.py
@@ -4,5 +4,5 @@ from backend.consumers.chat import ChatConsumer
     Route for the websocket connection
 '''
 websocket_urlpatterns = [
-    re_path(r'^ws/support/chat/(?P<chat_uuid>[0-9a-f-]{36})/$', ChatConsumer.as_asgi()),
+    re_path('ws/support/chat/<uuid:chat_uuid>', ChatConsumer.as_asgi()),
 ]
