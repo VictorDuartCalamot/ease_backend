f4978b5331b786595be196cddaaac7fadde3f197
Victor <victor.duart@ieselcalamot.com>
2024-05-17 21:15:10 +0200

    changed from re_path to path

--- backend/routing.py
+++ backend/routing.py
@@ -1,8 +1,8 @@
-from django.urls import re_path
+from django.urls import path
 from backend.consumers.chat import ChatConsumer
 '''
     Route for the websocket connection
 '''
 websocket_urlpatterns = [
-    re_path('ws/support/chat/<uuid:chat_uuid>/', ChatConsumer.as_asgi()),
+    path('ws/support/chat/<uuid:chat_uuid>/', ChatConsumer.as_asgi()),
 ]
