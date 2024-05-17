1d0e9f5c7f336f790960651d590fa7d50e62cab2
Victor <victor.duart@ieselcalamot.com>
2024-05-18 00:37:55 +0200

    debug?

--- backend/consumers/chat.py
+++ backend/consumers/chat.py
@@ -47,6 +47,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Send previous messages to the user (if any)
         try:
             messages = await self.get_chat_messages(self.chat_uuid)
+            print('messages',messages)
             if messages:
                 serialized_messages = ChatMessageSerializer(messages, many=True).data
                 await self.send(text_data=json.dumps(serialized_messages))
